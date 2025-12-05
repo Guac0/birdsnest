4b6f8421e1a8a4da2f370e9f809773a86f952ebb
Guac0 <YeetYeetGuac@gmail.com>
2025-12-05 13:42:06 -0500

    server - fix build.py

--- server/build.py
+++ server/build.py
@@ -16,8 +16,9 @@ BUILD_DIR = "build_assets"
 
 SERVER_FILE = "server.py"
 NUITKA_ARGS = [
+    #"--standalone",
     "--onefile",
-    "--python-flag=no_docstrings",
+    #"--python-flag=no_docstrings", # breaks sqlalchemy
 ]
 
 
@@ -140,6 +141,10 @@ def main():
         "python", "-m", "nuitka",
         SERVER_FILE,
         *NUITKA_ARGS,
+        #"--plugin-enable=flask",
+        #"--plugin-enable=sqlalchemy",
+        #"--include-module=sqlalchemy.orm.dependency",
+        #"--include-module=flask_sqlalchemy.dependency",
         f"--include-data-dir={BUILD_DIR}/templates=templates",
         f"--include-data-dir={BUILD_DIR}/static=static",
     ]
