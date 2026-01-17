a65287b94882b1461ad060a2be0ebf35f028bbec
Guac0 <YeetYeetGuac@gmail.com>
2026-01-16 19:31:37 -0500

    stabvest - update build to produce reduced py files

--- roles/stabvest_client/files/build.py
+++ roles/stabvest_client/files/build.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import re
 import platform
+import tokenize,io
 
 # -----------------------------
 # CONFIGURATION
@@ -64,11 +65,78 @@ def get_platform_dist():
     # Fallback for MacOS or unknown systems
     return (sys_platform, platform.release(), platform.version())
 
+
+def minify_py(py_code):
+    """
+    Returns 'source' minus comments and docstrings.
+    """
+    io_obj = io.StringIO(py_code)
+    out = ""
+    prev_toktype = tokenize.INDENT
+    last_lineno = -1
+    last_col = 0
+    for tok in tokenize.generate_tokens(io_obj.readline):
+        token_type = tok[0]
+        token_string = tok[1]
+        start_line, start_col = tok[2]
+        end_line, end_col = tok[3]
+        ltext = tok[4]
+        # The following two conditionals preserve indentation.
+        # This is necessary because we're not using tokenize.untokenize()
+        # (because it spits out code with copious amounts of oddly-placed
+        # whitespace).
+        if start_line > last_lineno:
+            last_col = 0
+        if start_col > last_col:
+            out += (" " * (start_col - last_col))
+        # Remove comments:
+        if token_type == tokenize.COMMENT:
+            pass
+        # This series of conditionals removes docstrings:
+        elif token_type == tokenize.STRING:
+            if prev_toktype != tokenize.INDENT:
+        # This is likely a docstring; double-check we're not inside an operator:
+                if prev_toktype != tokenize.NEWLINE:
+                    # Note regarding NEWLINE vs NL: The tokenize module
+                    # differentiates between newlines that start a new statement
+                    # and newlines inside of operators such as parens, brackes,
+                    # and curly braces.  Newlines inside of operators are
+                    # NEWLINE and newlines that start new code are NL.
+                    # Catch whole-module docstrings:
+                    if start_col > 0:
+                        # Unlabelled indentation means we're inside an operator
+                        out += token_string
+                    # Note regarding the INDENT token: The tokenize module does
+                    # not label indentation inside of an operator (parens,
+                    # brackets, and curly braces) as actual indentation.
+                    # For example:
+                    # def foo():
+                    #     "The spaces before this docstring are tokenize.INDENT"
+                    #     test = [
+                    #         "The spaces before this string do not get a token"
+                    #     ]
+        else:
+            out += token_string
+        prev_toktype = token_type
+        last_col = end_col
+        last_lineno = end_line
+    return "\n".join([line for line in out.splitlines() if line.strip()])
+
+def process_py_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        py = f.read()
+
+    py = minify_py(py)
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(py)
+
 # -----------------------------
 # MAIN BUILD PROCESS
 # -----------------------------
 def main():
 
+    """
     print("=== Running Nuitka ===")
     nuitka_cmd = [
         "python", "-m", "nuitka",
@@ -78,9 +146,14 @@ def main():
 
     print("Running:", " ".join(nuitka_cmd))
     subprocess.run(nuitka_cmd, check=True)
+    """
+
+    shutil.copy("agent.py", "agent_reduced.py")
+    process_py_file("agent_reduced.py")
 
     print("\n=== Build complete! ===")
 
+    """
     osname=""
     system = platform.system()
     simple=False
@@ -111,6 +184,7 @@ def main():
     except FileNotFoundError as E:
         print("Attempted to rename output file but could not locate it (possible unexpected file exception or Nuitka error)")
         print(f"Full error: {E}")
+    """
 
 if __name__ == "__main__":
     main()
--- roles/stabvest_server/files/build.py
+++ roles/stabvest_server/files/build.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 import re
 import platform
+import io,tokenize
 
 from htmlmin import minify as html_minify
 from jsmin import jsmin
@@ -14,6 +15,7 @@ from jsmin import jsmin
 SOURCE_TEMPLATES = "templates"
 SOURCE_STATIC = "static"
 BUILD_DIR = "build_assets"
+SOURCE_MISC = ["config.json","server.py","requirements.txt"]
 
 TARGET_FILE = "server.py"
 NUITKA_ARGS = [
@@ -54,6 +56,63 @@ def minify_css(css_code):
     css_code = re.sub(r"\s+", " ", css_code)                        # collapse whitespace
     css_code = re.sub(r"\s*([{}:;,])\s*", r"\1", css_code)          # tighten punctuation spacing
     return css_code.strip()
+
+
+def minify_py(py_code):
+    """
+    Returns 'source' minus comments and docstrings.
+    """
+    io_obj = io.StringIO(py_code)
+    out = ""
+    prev_toktype = tokenize.INDENT
+    last_lineno = -1
+    last_col = 0
+    for tok in tokenize.generate_tokens(io_obj.readline):
+        token_type = tok[0]
+        token_string = tok[1]
+        start_line, start_col = tok[2]
+        end_line, end_col = tok[3]
+        ltext = tok[4]
+        # The following two conditionals preserve indentation.
+        # This is necessary because we're not using tokenize.untokenize()
+        # (because it spits out code with copious amounts of oddly-placed
+        # whitespace).
+        if start_line > last_lineno:
+            last_col = 0
+        if start_col > last_col:
+            out += (" " * (start_col - last_col))
+        # Remove comments:
+        if token_type == tokenize.COMMENT:
+            pass
+        # This series of conditionals removes docstrings:
+        elif token_type == tokenize.STRING:
+            if prev_toktype != tokenize.INDENT:
+        # This is likely a docstring; double-check we're not inside an operator:
+                if prev_toktype != tokenize.NEWLINE:
+                    # Note regarding NEWLINE vs NL: The tokenize module
+                    # differentiates between newlines that start a new statement
+                    # and newlines inside of operators such as parens, brackes,
+                    # and curly braces.  Newlines inside of operators are
+                    # NEWLINE and newlines that start new code are NL.
+                    # Catch whole-module docstrings:
+                    if start_col > 0:
+                        # Unlabelled indentation means we're inside an operator
+                        out += token_string
+                    # Note regarding the INDENT token: The tokenize module does
+                    # not label indentation inside of an operator (parens,
+                    # brackets, and curly braces) as actual indentation.
+                    # For example:
+                    # def foo():
+                    #     "The spaces before this docstring are tokenize.INDENT"
+                    #     test = [
+                    #         "The spaces before this string do not get a token"
+                    #     ]
+        else:
+            out += token_string
+        prev_toktype = token_type
+        last_col = end_col
+        last_lineno = end_line
+    return "\n".join([line for line in out.splitlines() if line.strip()])
 
 
 def minify_inline_js(html):
@@ -108,6 +167,16 @@ def process_css_file(path):
         f.write(css)
 
 
+def process_py_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        py = f.read()
+
+    py = minify_py(py)
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(py)
+
+
 def minify_directory(dir_path):
     for root, _, files in os.walk(dir_path):
         for file in files:
@@ -121,6 +190,9 @@ def minify_directory(dir_path):
 
             elif file.endswith(".css"):
                 process_css_file(full_path)
+            
+            elif file.endswith(".py"):
+                process_py_file(full_path)
 
 def get_platform_dist():
     sys_platform = platform.system()
@@ -170,10 +242,13 @@ def main():
     print("=== Copying asset directories ===")
     copy_tree(SOURCE_TEMPLATES, os.path.join(BUILD_DIR, "templates"))
     copy_tree(SOURCE_STATIC, os.path.join(BUILD_DIR, "static"))
+    for f in SOURCE_MISC:
+        shutil.copy(f, BUILD_DIR)
 
     print("=== Minifying HTML, JS, and CSS assets ===")
     minify_directory(BUILD_DIR)
 
+    """
     print("=== Running Nuitka ===")
     nuitka_cmd = [
         "python", "-m", "nuitka",
@@ -191,9 +266,11 @@ def main():
 
     print("Running:", " ".join(nuitka_cmd))
     subprocess.run(nuitka_cmd, check=True)
+    """
 
     print("\n=== Build complete! ===")
 
+    """
     osname=""
     system = platform.system()
     simple=False
@@ -224,7 +301,7 @@ def main():
     except FileNotFoundError as E:
         print("Attempted to rename output file but could not locate it (possible unexpected file exception or Nuitka error)")
         print(f"Full error: {E}")
-
+    """
 
 if __name__ == "__main__":
     main()
